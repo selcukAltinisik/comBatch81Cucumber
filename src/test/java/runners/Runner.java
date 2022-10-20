f1c3e4b85b93dc844a11fb290eb388f032f82cb9
selcukAltinisik <selcukaltin3544@gmail.com>
2022-10-20 10:00:37 +0300

    20Eki22-10:00

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -19,7 +19,7 @@ public class Runner {
     /*
      Runner Class'ı TestNG'deki XMl mantığı ile çalışır. Çalıştırmak istediğimiz senartolara tag belirtiriz
     ve belirttiğimiz tag'ları çalıştırır. XMl deki gibi istediğimiz testleri çalıştırmak için kullanırız.
-    Runner class body'si boştur ve runner class'ını ekleyeceğimiz notasyonlar aktive eder.
+    Runner class body'si boştur ve runner class'ını ekleyeceğimiz notasyonlar aktive eder..
      Bu class'da kullanacağımız 2 adet notasyon vardır
     -@RunWith(Cucumber.class)  notasyonu Runner class'ına çalışma özelliği ekler.
     Bu notasyon olduğu için Cucumber frameworkumuzde Junit kullanmayı tercih ederiz
